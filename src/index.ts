d27d16563c6597ea30799d45e648d597f7f135b9
ujun <zhaorujunfor@tianzehuitong.com>
2018-06-21 17:59:49 +0800

    add  rx

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+declare const wx:any;
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/do";
+
 export function initGame() {
+	const canvas = wx.createCanvas();
+	Observable.of(1, 2, 3)
+		.map(x => x + "!!!")
+		.do(x => {
+			console.log(x);
+		})
+		.subscribe(next => {});
+
 	return "begin";
 }
