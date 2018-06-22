0518e9f33f800be20ddabfc25f87746ed869f98c
ujun <zhaorujunfor@tianzehuitong.com>
2018-06-22 13:43:51 +0800

    update

---
+++ src/canstant.ts
@@ -0,0 +1,9 @@
+declare const wx:any
+
+export const WINDOW_WIDTH=wx.getSystemInfoSync().windowWidth
+
+export const WINDOW_HEIGHT=wx.getSystemInfoSync().windowHeight
+
+export const CANVAS_WIDTH=WINDOW_WIDTH;
+
+export const CANVAS_HEIGHT=WINDOW_HEIGHT
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 declare const wx:any;
 
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Observable";
 import "rxjs/add/observable/of";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/do";
 
+import {renderCanvas} from './renderCanvas'
+
 export function initGame() {
 	const canvas = wx.createCanvas();
+	renderCanvas(canvas);
 	Observable.of(1, 2, 3)
 		.map(x => x + "!!!")
-		.do(x => {
-			console.log(x);
-		})
-		.subscribe(next => {});
+		.subscribe(next => {
+			console.log(next);
+		});
 
 	return "begin";
 }
+
